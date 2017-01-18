736b435c68f3d06ca3955bae23385020152e5dc7
Sarah Miller <sarahmiller@Sarahs-MacBook-Pro.local>
2017-01-18 17:21:26 +0000

    added cards as divs using JS

--- starter-code/main.js
+++ starter-code/main.js
@@ -5,9 +5,25 @@ var cardTwo = "queen";
 var cardThree="king";
 var cardFour="king";
 
-if  (cardOne === cardTwo) {
+//function to create cards in JS//
+var createCards=function(){
+
+var gameBoard =document.getElementById ('game-board');
+//counter creates 4 cards//
+for (var i = 0; i <= 3; i++) {
+//creates new var and new div//	
+var cards = document.createElement ('div');
+//creates class in CSS//
+cards.className="card";
+//appends cards to board//
+document.querySelector('div').appendChild(cards);
+}
+}
+//calls card creating function//
+createCards ();
+	/*1  (cardOne === cardTwo) {
 alert ('Congratualtions, you found a match');
 }
 else {
 alert ("Sorry your cards don't match, better luck next time");
-}
+}*/
